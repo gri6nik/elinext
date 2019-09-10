d176d09bc83df88b47cdb582312054c451a0c17c
Igor <pochtamoyatyt6@gmail.com>
2019-09-10 17:43:04 +0300

    feat: create project

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by User on 9/10/19.
+//  Copyright © 2019 TestCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
